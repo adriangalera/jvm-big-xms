d8193bdfc888cf2244acf9c0ce0e5acca70d90ca
Adrian Galera <adrian.galera@ocado.com>
2020-02-03 17:59:58 +0100

    Initial import

---
+++ src/main/java/Boom.java
@@ -0,0 +1,9 @@
+public class Boom {
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println("I'm here");
+            Thread.sleep(1000);
+        }
+    }
+}
